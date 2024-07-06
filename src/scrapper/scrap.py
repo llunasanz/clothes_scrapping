327e3e148c19cc09ab5d956dfda8442d50f0cb4f
llunasanz <lluna@declarando.es>
2024-07-06 12:16:13 +0200

    MOre optimization and modular organization

--- src/scrapper/scrap.py
+++ src/scrapper/scrap.py
@@ -28,32 +28,71 @@ class_to_key = {
 
 
 def split_currency_amount(s):
-    currency = ''.join(filter(lambda char: not char.isdigit() and char != '.', s)).replace(" ", "")
-    amount = float(''.join(filter(lambda char: char.isdigit() or char == '.', s)))
+    currency = ''.join(filter(
+        lambda char: not char.isdigit() and char != '.',
+        s
+    )).replace(" ", "")
+    amount = float(''.join(filter(
+        lambda char: char.isdigit() or char == '.',
+        s
+    )))
     return currency, amount
 
 size_order = ["XXXS", "XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"]
+size_index = dict(map(lambda x: (x[1], x[0]), enumerate(size_order)))
+size_pattern = re.compile(r'\b(?:XXXS|XXS|XS|S|M|L|XL|XXL|XXXL)\b')
 
 
 def sort_sizes(sizes):
     # Sort sizes based on their position in the predefined order
-    size_index = size_index = dict(map(lambda x: (x[1], x[0]), enumerate(size_order)))
-    return sorted(sizes, key=lambda size: size_index.get(size, len(size_order)))
+    size_index = size_index = dict(map(
+        lambda x: (x[1], x[0]),
+        enumerate(size_order)
+    ))
+    return sorted(
+        sizes, key=lambda size: size_index.get(size, len(size_order))
+    )
 
 
 def extract_images(soup):
     list_items = soup.find_all('img')
     product_name = soup.find('h1', class_='ProductMeta__Title').text.strip()
     matching_images_0 = list(filter(None, map(
-        lambda x: x.get('data-original-src') if x.get('alt') == product_name else '', 
+        lambda x: x.get('data-original-src')
+            if x.get('alt') == product_name
+            else '', 
         list_items
     )))
     matching_images_1 = list(filter(None, map(
-        lambda x: x.get('src') if x.get('alt') == product_name else '', 
+        lambda x: x.get('src')
+            if x.get('alt') == product_name
+            else '', 
         list_items
     )))
     matching_images = matching_images_0 + matching_images_1
     return list(map(lambda x: 'https:' + x, matching_images))
+
+
+def extract_sizes(soup):
+    options = soup.find_all('option')
+    sizes = filter(None, map(
+        lambda x: size_pattern.search(x.text.strip()).group(0)
+            if size_pattern.search(x.text.strip())
+            else '',
+            options
+        ))
+    return sort_sizes(list(set(sizes)))
+
+
+def extract_colours(soup):
+    options = soup.find_all('option')
+    colours = filter(None, map(
+        lambda x: x.text.split(' - ')[0].strip().split('/')[0].strip()
+            if size_pattern.search(x.text.strip())
+            else '',
+            options
+        ))
+    return list(set(colours))
 
 
 def extract_data(soup, class_name):
@@ -61,24 +100,14 @@ def extract_data(soup, class_name):
     tag_name = class_to_tag.get(class_name)
 
     # Take care about special cases (images, sizes and colours)
-    if class_name == 'img':
-        return extract_images(soup)
-
-    size_pattern = re.compile(r'\b(?:XXXS|XXS|XS|S|M|L|XL|XXL|XXXL)\b')
-
-    if class_name == 'sizes':
-        sizes = list(filter(None, map(
-            lambda x: size_pattern.search(x.text.strip()).group(0) if size_pattern.search(x.text.strip()) else '', 
-            soup.find_all('option')
-        )))
-        return sort_sizes(list(set(sizes)))
-
-    if class_name == 'colours':
-        colours = list(filter(None, map(
-            lambda x: x.text.split(' - ')[0].strip().split('/')[0].strip() if size_pattern.search(x.text.strip()) else '', 
-            soup.find_all('option')
-        )))
-        return list(set(colours))    
+    special_cases = {
+        'img': lambda: extract_images(soup),
+        'sizes': lambda: extract_sizes(soup),
+        'colours': lambda: extract_colours(soup)
+    }
+    
+    if class_name in special_cases:
+        return special_cases[class_name]()
 
     # Find the element with the given class name and tag
     element = soup.find(tag_name, class_=class_name)
